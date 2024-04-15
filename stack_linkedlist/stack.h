e1efd4727744c96ee01de7e6b669459919bd2af1
ecoses042 <ecoses042@gmail.com>
2024-04-15 14:32:36 +0900

    improved linked list

---
+++ stack_linkedlist/stack.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+template<typename stack_type>
+class stack{
+    public:
+        stack();
+        ~stack();
+        void push(const stack_type& value);
+        stack_type pop();
+        stack_type top();
+        bool isempty();
+        bool operation(char stack_operator);
+    private:
+        struct Node;
+        typedef Node *link;
+        struct Node{
+            stack_type element;
+            link next;
+        };
+        link head;
+        link tail;
+};
+
+template<typename stack_type>
+stack<stack_type>::stack()
+{
+    head = NULL;
+    tail = NULL;
+}
+
+template<typename stack_type>
+stack<stack_type>::~stack()
+{
+    while(isempty())
+        pop();
+}
+template<typename stack_type>
+void stack<stack_type>::push(const stack_type& value)
+{
+    link addednode = new Node;
+    assert(addednode);
+    addednode->element = value;
+    if (head == NULL)
+    {
+        tail = addednode;
+    }
+    addednode->next = head;
+    head = addednode;
+}
+
+template<typename stack_type>
+stack_type stack<stack_type>::pop()
+{
+    assert(head);
+    link removenode(head);
+    head = removenode->next;
+    return (removenode->element);
+}
+
+template<typename stack_type>
+stack_type stack<stack_type>::top()
+{
+    assert(head);
+    return (head->element);
+}
+
+template<typename stack_type>
+bool stack<stack_type>::isempty()
+{
+    return (bool(head));
+}
+
